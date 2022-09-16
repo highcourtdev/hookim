6cd413ced874a96429167b04f2136ad3aa967630
highcourtdev <highcourtmaharajaoffice1998@gmail.com>
2022-09-16 10:25:11 +0530

    useState learn

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseState = () => {
+    const[test, testUpdate] = useState("Highcourt");
+
+    const fnTest = () =>{
+        if(!test){
+            testUpdate("Maharaja");
+        }
+        
+    }
+    
   return (
-    <div>UseState</div>
+    <div>
+        <h1>{Date.now()}</h1>
+        <button onClick={fnTest}></button>
+    
+    </div>
   )
 }
 
